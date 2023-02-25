2d4bc527e9c1df01dc9f091654aa625b86323ba1
shefmaster <andrej.kozik@gmail.com>
2023-02-25 21:21:34 +0100

    updated start package

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -18,36 +18,4 @@ export class ValidationPipe implements PipeTransform<any> {
     }
     return value;
   }
-
 }
-
-
-
-
-
-
-
-
-
-// import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common";
-// import { validate } from "class-validator";
-// import { ValidationException } from "../exceptions/validation.exeption";
-// import { plainToClass } from "class-transformer";
-//
-//
-// @Injectable()
-// export class ValidationPipe implements PipeTransform<any> {
-//   async transform(value: any, metadata: ArgumentMetadata): Promise<any > {
-//     const obj = plainToClass(metadata.metatype, value)
-//     const errors = await validate(obj)
-//
-//     if(errors.length) {
-//       console.log(errors)
-//       // let messages = errors.map(err => {
-//       //   return `${err.property} - `
-//       // })
-//       throw new ValidationException('');
-//     }
-//     return value;
-//   }
-// }
